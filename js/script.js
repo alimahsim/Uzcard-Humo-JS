849dd098a1f1d98e7eb0ee6956a6681ad49b7911
alimahsim <alimahsim@gmail.com>
2023-01-27 17:49:33 +0500

    complite program

--- js/script.js
+++ js/script.js
@@ -3,36 +3,18 @@ console.log("Hello world!");
 
 // Recognizes cards
 
-function getCardNum(num){
+let cardNum = '9860120185337800a'
+function showNum(){
+if (cardNum.startsWith('8600') && cardNum.length == 16){
+    console.log("You've entered Uzcard , the number is:");
+    console.log(cardNum.slice(0,4) + '********' + cardNum.slice(12,16))
+   
+}if(cardNum.startsWith('9860') && cardNum.length == 16){
+    console.log("You've entered Humo card , the number is:");
+    console.log(cardNum.slice(0,4) + '********' + cardNum.slice(12,16))
 
-    if (num.startsWith('8600') && num.length === 16){
-            console.log(`You've entered Humo card , the number is: ${num}`);
-
-    }else if(num.startsWith('9860') && num.length === 16){
-            console.log(`You've entered Uzcard , the number is: ${num}`);
-
-    }else{
-        if(num == NaN && num.length !== 16){
-            console.log("You've entered rubbish mate!")
-        }
-    }   
-}
-
-let result = getCardNum();
-console.log(result);
-
-/*
-function cardMask(num, char = "*"){
-    num = num.replace(/[^0-9]+/g, '');
-    let len = num.length;
-    return num.substring(0,4) + char.repeat(l-8) + num.substring(len-4,len);
-}
-cardMask();
-*/
-
-/*
-let sentences = "I with could have done it";
-console.log(sentences.startsWith('I'));
-
-*/
+}else{
+    console.log("You've entered rubbish mate!")
+}}
+showNum()
 
